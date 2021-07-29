14582b3ef895b0a26d81df5c64e18bcfe56e4c1c
Jean Grijp <grijpjean@gmail.com>
2021-07-29 10:17:59 -0300

    .

---
+++ src/components/Navbar/style.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const Navbar = styled.nav`
+  width: 100%;
+  max-width: 1000px;
+  height: 8vh;
+  max-height: 8vh;
+  background-color: #d4ebd4;
+`;
+
